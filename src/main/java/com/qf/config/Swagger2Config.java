01385813c08427815d606682806b9901145037ca
Tian <403483127@qq.com>
2020-12-09 22:19:51 +0800

    xiugai

--- src/main/java/com/qf/config/Swagger2Config.java
+++ src/main/java/com/qf/config/Swagger2Config.java
@@ -2,6 +2,7 @@ package com.qf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
--- src/main/java/com/qf/controller/UserController.java
+++ src/main/java/com/qf/controller/UserController.java
@@ -4,11 +4,9 @@ import com.qf.common.BaseResp;
 import com.qf.pojo.User;
 import com.qf.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
 public class UserController {
